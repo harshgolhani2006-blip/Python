d5b84ffc2f237e3abb8e3e1060d47584577f2746
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-04-02 10:24:55 +0530

    Basic Python.

---
+++ Class/first.py
@@ -0,0 +1,25 @@
+# print("Hello, World!")
+#wap to print the id of the variables
+# x=5;
+# y=7;
+# z=5;
+# print(id(x),id(y),id(z));
+
+#wap to print the type of the variables
+# x=5;
+# y=7.5;
+# z="Hello";
+# print(type(x),type(y),type(z));
+
+#wap to get input from the user and print it.
+# Name=input("Enter your name: ");
+# age=int(input("Enter your age: "));
+# height=float(input("Enter your height: "));
+# print("My name is " + Name,"I am " + str(age) + " years old, and my height is " + str(height) + " cm.");
+
+#wap to print area and perimeter of a rectangle, and input length and breadth from the user.
+length=float(input("Enter the length of the rectangle: "));
+breadth=float(input("Enter the breadth of the rectangle: "));
+area=length*breadth;
+perimeter=2*(length+breadth);
+print("The area of the rectangle is: " + str(area) + " and the perimeter of the rectangle is: " + str(perimeter));
